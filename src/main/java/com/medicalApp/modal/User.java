84c71c0be67612c2913867e1ceafa2ee5fede7b0
Junior Kengne <kengnejunior@gmail.com>
2023-12-10 12:02:35 +0200

    new changes

--- src/main/java/com/medicalApp/modal/User.java
+++ src/main/java/com/medicalApp/modal/User.java
@@ -1,12 +1,10 @@
 package com.medicalApp.modal;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
